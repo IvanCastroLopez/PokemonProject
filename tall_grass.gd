9478aefc2f084047197701c3c4a2b560fa7c1af4
IvanCastroLopez <116338591+IvanCastroLopez@users.noreply.github.com>
2023-11-22 13:35:24 +0100

    Trying to fix grass overlay

--- tall_grass.gd
+++ tall_grass.gd
@@ -1,18 +1,33 @@
 extends Node2D
 
-@onready var anim_player = $AnimationPlayer
-const grass_overlay_texture = preload("res://Assets/Grass/stepped_tall_grass.png")
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
+const grass_overlay_texture: Texture = preload("res://Assets/Grass/stepped_tall_grass.png")
 var grass_overlay: TextureRect = null
 
 var player_inside: bool = false
 
-# Player exiting grass
-func _on_player_player_moving_signal():
-	player_inside = false
+func _ready():
+	# Find the Player node in the common ancestor (Town Scene)
+	var player_node = %Player
+	
+	# Connect to signals emitted by the Player node
+	if player_node:
+		player_node.player_moving_signal.connect("_player_exiting_grass")
+		player_node.player_stopped_signal.connect("_player_entered_grass")
 
-# Player in grass
-func _on_player_player_stopped_signal():
-	pass
+func _player_exiting_grass():
+	player_inside = false
+	if is_instance_valid(grass_overlay):
+		grass_overlay.queue_free()
+	
+func _player_entered_grass():
+	if player_inside:
+		grass_overlay = TextureRect.new()
+		grass_overlay.texture = grass_overlay_texture
+		grass_overlay.rect_position = position
+		get_tree().current_scene.add_child(grass_overlay)
 
 func _on_area_2d_body_entered(body):
-	anim_player.play("Stepped")
+	if body.is_in_group("Player"):  # Check if the entering body is the Player
+		player_inside = true
+		anim_player.play("Stepped")
